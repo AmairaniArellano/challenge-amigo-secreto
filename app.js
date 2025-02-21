228474fd01da92315f144a23fd4bd4f2fb6016d7
Amairani Arellano <lyliarellano@gmail.com>
2025-02-21 12:58:03 -0600

    función para actualizar la lista de amigos en html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = [];
 let ingresarAmigos = document.getElementById("amigo");
+let amigosHtml = document.getElementById("listaAmigos");
 
 
 function agregarAmigo() {
@@ -10,5 +11,13 @@ function agregarAmigo() {
         listaAmigos.push(ingresarAmigos.value);// Agregar nombre a la lista
         console.log(listaAmigos); //Verificar el arreglo
         ingresarAmigos.value = ''; //Limpiar el campo de texto después de agregar
+    }
+
+    // Limpiar la lista HTML antes de agregar nuevos elementos
+    amigosHtml.innerHTML = ""
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+        // Concatenar cada <li> con el nombre de los amigos
+        amigosHtml.innerHTML += `<li>${listaAmigos[i]}</li>`;
     }
 }
