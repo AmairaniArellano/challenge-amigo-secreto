e6540833e90b5272c3be3ebb207670d77262cf21
Amairani Arellano <lyliarellano@gmail.com>
2025-02-20 20:46:44 -0600

    función agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let ingresarAmigos = document.getElementById("amigo");
+
+
+function agregarAmigo() {
+    if (ingresarAmigos.value == '') {
+        alert('Por favor, ingrese un nombre');
+    } else { 
+        listaAmigos.push(ingresarAmigos.value);// Agregar nombre a la lista
+        console.log(listaAmigos); //Verificar el arreglo
+        ingresarAmigos.value = ''; //Limpiar el campo de texto después de agregar
+    }
+}
